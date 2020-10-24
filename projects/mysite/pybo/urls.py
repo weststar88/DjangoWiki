1679481f31f505a540935e185f400ca26d3d8a45
weststar88 <65124101+weststar88@users.noreply.github.com>
2020-10-24 22:05:39 +0900

    2-4
    2-4

--- projects/mysite/pybo/urls.py
+++ projects/mysite/pybo/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('1/', views.test),
+    path('<int:question_id>/', views.detail),
 ]
--- projects/mysite/pybo/views.py
+++ projects/mysite/pybo/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+    """
+    pybo 목록 출력
+    """
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
 
-def test(request):
-    return HttpResponse("test 입니다.")
+def detail(request, question_id):
+    """
+    pybo 내용 출력
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
